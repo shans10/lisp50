190241ab92004b88d5752d25c6b6bdf185ee2df6
shans10 <shantanu.shukla10@gmail.com>
2021-02-21 21:10:17 +0530

    completed chapter 7

--- evaluation.c
+++ evaluation.c
@@ -28,6 +28,38 @@ void add_history(char* unused) {}
 #include <editline/readline.h>
 #endif
 
+// Use operator string to see which operation to perform.
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    // If tagged as number return it directly.
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // The operator is always second child.
+    char* op = t->children[1]->contents;
+
+    // We store the third child in `x`
+    long x = eval(t->children[2]);
+
+    // Iterate the remaining children and combining.
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     /* Create Some Parsers */
