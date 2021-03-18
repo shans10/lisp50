4cab2bb10f57b7aeb6487e493314f6a1d4195210
Shantanu Shukla <shantanu.shukla10@gmail.com>
2021-03-18 22:37:53 +0530

    started chapter-8 error handling

---
+++ error_handling.c
@@ -0,0 +1,49 @@
+#include "mpc.h"
+
+#ifdef _WIN32
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#endif
+
+/* Create Enumeration of Possible Error Types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create Enumeration of Possible lval Types */
+enum { LVAL_NUM, LVAL_ERR };
+
+/* Declare new lval struct */
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* Create a new number type lval */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+/* Create a new error type lval */
+lval lval_err(int x) {
+    lval v;
+    v.type =  LVAL_ERR;
+    v.err = x;
+    return v;
+}
